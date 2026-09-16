7df5b2282f9ce894915d2ae081e78bf2cd160bb9
Linannyu <lin2yi5007@gmail.com>
2026-09-15 23:54:34 -0400

    Day55:Update

--- APCSA_Work/Sketch.java
+++ APCSA_Work/Sketch.java
@@ -16,23 +16,36 @@ public class Sketch extends PApplet {
         background(220);
         textSize(18);
         fill(255, 0, 0);
+        strokeWeight(1);
+        stroke(0, 0, 0);
         text(mouseX + " " + mouseY, 50, 50);
         // antenna
         fill(0xFFf9dfc4);
         triangle(310, 40, 270, 95, 350, 95);
 
+
         // head
         fill(0xFFf9dfc4);
         ellipse(310, 165, 200, 150);
 
-        // ear
-        fill(0xFFf9dfc4);
+        // left ear
+        noStroke();
+        fill(0xFFFFD4A9);
         ellipse(210, 165, 30, 50);
-        ellipse(410, 165, 30, 80);
-
+        fill(0xFF854e27);
         ellipse(200, 165, 20, 40);
+        fill(0xFF644030);
+        ellipse(190, 165, 15, 30);
+        // right ear
+        fill(0xFFFFD4A9);
+        ellipse(410, 165, 30, 50);
+        fill(0xFF854e27);
+        ellipse(420, 165, 20, 40);
+        fill(0xFF644030);
+        ellipse(430, 165, 15, 30);
 
         // left leg
+        stroke(0 ,0 ,0);
         fill(0xFFf9dfc4);
         rect(270, 340, 25, 60);
         // right leg
@@ -43,17 +56,21 @@ public class Sketch extends PApplet {
         rect(250, 225, 120, 125, 0, 0, 30, 30);
 
         // feet
+        stroke(0, 0, 0);
         rect(255, 390, 43, 25, 30, 0, 0, 2);
         rect(322, 390, 43, 25, 0, 30, 2, 0);
 
         // arm
-        ellipse(243, 270, 25, 80);
-        circle(243, 310, 30);
+        fill(0xFFf9dfc4);
+        ellipse(235, 270, 25, 80);
+        ellipse(385, 270, 25, 80);
 
-        ellipse(377, 270, 25, 80);
-        circle(377, 310, 30);
+        fill(0xFFFFFFFF);
+        circle(386, 310, 30);
+        circle(235, 310, 30);
 
         //eyes
+        strokeWeight(1);
         fill(0xFFae7145);
         circle(275, 140, 30);
         circle(345, 140, 30);
@@ -66,6 +83,7 @@ public class Sketch extends PApplet {
         fill(0xFFFFFFFF);
         circle(277, 138, 5);
         circle(347, 138, 5);
+
         // nose
         fill(0xFFdbba91);
         circle(310, 140, 20);
@@ -78,8 +96,20 @@ public class Sketch extends PApplet {
         fill(0xFFebd6cb);
         ellipse(310, 285, 90, 100);
 
-        // button
+        // pocket
+        fill(255, 0, 0);
+        rect(280, 280, 60, 30);
 
+        // 
+        fill(0xFFf9dfc4);
+        square(220,225,30);
+        square(370,225,30);
+
+        // button
+        fill(0xFFEFEFFF);
+        strokeWeight(3);
+        circle(310, 255, 20);
+    
 
 
 
--- README.md
+++ README.md
@@ -59,3 +59,4 @@
 | Day 52 | Java_for_Python_APCSA | Day07 | 完成：练习题 | 09/12/2026 |
 | Day 53 | Java_for_Python_APCSA | Day08 | 数组 | 09/13/2026 |
 | Day 54 | APCSA_Work | 01. My Robot | 画图制作robot | 09/14/2026 |
+| Day 55 | APCSA_Work | 02. My Robot | Update | 09/15/2026 |
